7db69edf21238b1f13541e4ae90b62d614b7b69a
dharshi <dharshib.8@gmail.com>
2025-03-17 12:06:18 +0530

    Add readme file

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Express } from 'express';
 import cors from "cors";
 import { Server } from "node:http";
 import { runBalServer } from './bal_ls';
-import fsRouter, { BASE_DIR, BASE_DIR_1 } from "./file_system/fsRoutes"
+import fsRouter, { BASE_DIR } from "./file_system/fsRoutes"
 import balRouter from "./file_system/balRoutes"
 
 const app: Express = express();
@@ -16,9 +16,7 @@ app.use("/bala", balRouter);
 const httpServer: Server = app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
     console.log("base dir: ", BASE_DIR);
-    console.log("base dir: ", BASE_DIR_1);
 
 });
-
 
 runBalServer(httpServer);
